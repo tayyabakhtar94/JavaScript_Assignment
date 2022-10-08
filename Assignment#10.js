fba5bd0d2e52556dfb82050710ae7cd3e7edeb84
Tayyab Akhtar <tayyabakhtar94@gmail.com>
2022-10-09 01:10:00 +0500

    Add files via upload

---
+++ Assignment#10.js
@@ -0,0 +1,8 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+
+// At Date: 10/9/2022 | Name: Tayyab | This is calculation on console command
+console.log("8 + 2 =",8+2);
+
+// At Date: 10/9/2022 | Name: Tayyab | This is simple text on console command
+console.log("This is simple text");
