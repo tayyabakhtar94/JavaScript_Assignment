729cbfff4387365d26ab703eb6005682d97ab6e3
Tayyab Akhtar <tayyabakhtar94@gmail.com>
2022-10-08 22:54:44 +0500

    Add files via upload

---
+++ Assignment#2.js
@@ -0,0 +1,4 @@
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+var HendrithVanlan = "When new things emerge in our world, its best to put some time into researching them and trying to gain an understanding. With that understanding, you're then able to think about and plan for the new business applications for those things and the new ways in which your business may profit from them."
+console.log(HendrithVanlan);
