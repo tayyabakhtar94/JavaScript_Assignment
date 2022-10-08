5c7b09c19a418afd0d94ff377de0e063534a68df
Tayyab Akhtar <tayyabakhtar94@gmail.com>
2022-10-09 01:31:43 +0500

    Add files via upload

---
+++ Assignment#12.js
@@ -0,0 +1,9 @@
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+var names = ["Yasir","Ather","Salman","Abdullah"];
+
+// printing names of each element
+console.log( names[0]+" Jigar hai apna!");
+console.log( names[1]+" Jigar hai apna!");
+console.log( names[2]+" Jigar hai apna!");
+console.log( names[3]+" Jigar hai apna!");
