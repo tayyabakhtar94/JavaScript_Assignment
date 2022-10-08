8b262ae60bd75ed9abfdfcb56e08fde295435f15
Tayyab Akhtar <tayyabakhtar94@gmail.com>
2022-10-09 01:01:44 +0500

    Add files via upload

---
+++ Assignment#9.js
@@ -0,0 +1,5 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+var favorite_number = 2;
+var quote = 'There are '+favorite_number+' types of people in this world,Those concerned about doing the work — and those concerned about getting the credit.';
+console.log(quote);
