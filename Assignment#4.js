8a2d30e07656e12bf14d80fecae6f370e8e16869
Tayyab Akhtar <tayyabakhtar94@gmail.com>
2022-10-09 00:06:08 +0500

    Add files via upload

---
+++ Assignment#4.js
@@ -0,0 +1,5 @@
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+var quote = 'Carl Sagan once said, "We live in a society exquisitely dependent on science and technology, in which hardly anyone knows anything about science and technology."';
+console.log(quote);
